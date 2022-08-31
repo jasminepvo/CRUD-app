b70bea42246c136aa0424daff5035ad4fb8f8d4a
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-31 17:26:43 -0400

    Set up READ operation with GET requeest, also set up express, node, nodemon

---
+++ server.js
@@ -0,0 +1,22 @@
+//test if node is running properly
+console.log("May Node be with You");
+
+//this is how we use express by requiring express
+const express = require("express");
+const app = express();
+
+//create a server that browsers can connect to using listen method
+app.listen(3000, function () {
+	console.log("listening on 3000");
+});
+
+//handle the get request with the get method
+//app.get(endpoint,callback)
+//endpoint is /
+//callback tells the server what to do when the requested endpoint matches the endpoint stated and takes two objects: request and response
+app.get("/", (req, res) => {
+	//__dirname is the current directory you're in
+	res.sendFile(__dirname + "/index.html");
+});
+
+//serve up an index.html page back to the browser using sendFile method provided by the res object
