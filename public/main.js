3845025bbddf72c189ae9c686f42aee48c822acb
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-06 19:09:20 -0400

    send the put request

--- public/main.js
+++ public/main.js
@@ -3,4 +3,14 @@ const update = document.querySelector("#update-button");
 
 update.addEventListener("click", (_) => {
 	// Send PUT Request here
+	fetch("/quotes", {
+		method: "put",
+		//tell the server we're sending JSON data by setting the content-type
+		headers: { "Content-Type": "application/json" },
+		//convert the data we send into JSON
+		body: JSON.stringify({
+			name: "Darth Vader",
+			quote: "I find your lack of faith disturbing.",
+		}),
+	});
 });
