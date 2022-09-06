1f2acf62f02f73e87177a5cf5a9f0fa9652c81f4
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-06 19:31:30 -0400

    add delete button and triger delete request through fetch when user clicks delete button

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 // main.js
 const update = document.querySelector("#update-button");
+const deleteButton = document.querySelector("#delete-button");
 
 update.addEventListener("click", (_) => {
 	// Send PUT Request here
@@ -18,5 +19,21 @@ update.addEventListener("click", (_) => {
 		})
 		.then((response) => {
 			window.location.reload(true);
+		});
+});
+
+deleteButton.addEventListener("click", (_) => {
+	fetch("quotes", {
+		method: "delete",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({
+			name: "Darth Vader",
+		}),
+	})
+		.then((res) => {
+			if (res.ok) return res.json();
+		})
+		.then((data) => {
+			window.location.reload();
 		});
 });
