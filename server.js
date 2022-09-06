101ef55ffc0397b3e7c1945e3d62687a7d68c5a9
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-06 19:38:27 -0400

    use deletedCount method to check if there are any more darth vader quotes to delete

--- server.js
+++ server.js
@@ -105,6 +105,9 @@ MongoClient.connect(
 			quotesCollection
 				.deleteOne({ name: req.body.name })
 				.then((result) => {
+					if (result.deletedCount === 0) {
+						return res.json("No quotes to delete");
+					}
 					res.json("Deleted Darth Vader's quote");
 				})
 				.catch((error) => console.error(error));
